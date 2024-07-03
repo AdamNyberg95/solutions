0f54e35bf80be7e1a6a94b3f4f56617de2dc97f2
Saraasso <sara@qte.se>
2024-07-04 00:32:11 +0200

    started doing about-section

--- app/about/page.styled.tsx
+++ app/about/page.styled.tsx
@@ -4,4 +4,42 @@ export const StyledSection = styled.section`
   max-width: 1200px;
   width: 100%;
   padding: 150px 0px 0px 0px;
+  height: 100vh;
 `;
+
+export const Wrapper = styled.div`
+display: flex;
+flex-direction: column;
+padding: 50px 50px;
+`;
+
+export const TextWrapper  = styled.div`
+
+`;
+
+export const CardWrapper = styled.div`
+display: flex;
+flex-direction: row;
+justify-content: center;
+`;
+
+export const Card = styled.div`
+max-width: 550px;
+margin: 20px 20px;
+
+`;
+
+export const CardTextWrapper = styled.div`
+margin: 4px 4px;
+
+
+`;
+
+export const Text1Wrapper = styled.div`
+
+border-bottom: solid 1px;
+border-color: grey;
+max-width: 200px;
+
+`;
+
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,13 +1,48 @@
 'use client';
 
 import React from 'react';
-import { StyledSection } from './page.styled';
+import { Card, CardWrapper, StyledSection, TextWrapper, Wrapper, CardTextWrapper, Text1Wrapper } from './page.styled';
+import { H2, TypographyBigThin } from '../components/Hero/Hero.styled';
+import { Hero } from '../components/Hero/Hero';
+import saraImg from '../../../../image/Sara-2.jpg'
+import Image from 'next/image';
+
 
 const About: React.FC = () => {
-  return (
+  return ( <>
+      
     <StyledSection>
-      <h1>Om oss</h1>
+
+      <Wrapper>
+<TextWrapper>
+      <H2>Vi är SA Solutions</H2>
+      <TypographyBigThin>Vi är två Frontend utvecklare med olika bakgrund inom frontend utveckling som tillsammans med dig vill skapa smarta weblösningar för ditt företag. Vi vill hjäla dig att digitalisera ditt företag och hålla din web i framkant med dagens tekonologi.</TypographyBigThin>
+      </TextWrapper>
+      </Wrapper>
+
+      <CardWrapper>
+        <Card>
+          <Image src={saraImg} alt="profile-img" width={300} height={300} ></Image>
+          <CardTextWrapper>
+
+          <text >Sara Södergård <br></br>Frontend utvecklare</text>
+          </CardTextWrapper>
+        </Card>
+        <Card>
+           <Image src={saraImg} alt="profile-img" width={300} height={300} ></Image>
+           <CardTextWrapper>
+            <text>Adam Nyberg <br></br>Frontend utvecklare</text>
+            </CardTextWrapper>
+        </Card>
+      </CardWrapper>
+
+
+
+      
+
+
     </StyledSection>
+    </>
   );
 };
 
