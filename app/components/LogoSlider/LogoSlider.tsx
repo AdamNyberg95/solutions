25bb8df13539762f77b6010f46c405bcd1924cab
adam nyberg <adamnyberg@MBPsomtlhoradam.lan>
2024-07-17 09:51:44 +0300

    chnages

--- app/components/LogoSlider/LogoSlider.tsx
+++ app/components/LogoSlider/LogoSlider.tsx
@@ -11,7 +11,7 @@ interface LogoData {
 const logos: LogoData[] = [
   { id: 1, src: '/next.svg', alt: 'Logo 1' },
   { id: 2, src: '/next.svg', alt: 'Logo 2' },
-  { id: 3, src: '/next.svg', alt: 'Logo 3' },
+  { id: 3, src: '/mysql.png', alt: 'Logo 3' },
   { id: 4, src: '/typescript.svg', alt: 'Logo 4' },
   { id: 5, src: '/figma.svg', alt: 'Logo 5' },
   { id: 6, src: '/next.svg', alt: 'Logo 6' },
